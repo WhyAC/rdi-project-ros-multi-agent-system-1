dbece15e8bff5e556a9f69420ea39fa0a486ebd1
WhyAC <51745911+WhyAC@users.noreply.github.com>
2026-04-15 11:37:56 +0700

    add some build files and folders

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/phiet/fix_robo/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/phiet/catkin_why/devel;/home/phiet/catkin_ws/devel;/opt/ros/indigo".split(';') if "/home/phiet/catkin_why/devel;/home/phiet/catkin_ws/devel;/opt/ros/indigo" != "" else []
