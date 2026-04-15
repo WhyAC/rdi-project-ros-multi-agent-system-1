14867906c1096356e0f6c1e9e0ead01d3f3eda02
WhyAC <51745911+WhyAC@users.noreply.github.com>
2026-04-15 11:41:25 +0700

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/phiet/fix_robo/src
+# Build directory: /home/phiet/fix_robo/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(minilab_simulation)
+SUBDIRS(minilab_navigation)
